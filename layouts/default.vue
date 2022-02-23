24f648efb6e4d3d00ea951fec7b8ecbb9fa6088c
Tommy Alhamra <tommy.alhamra@gmail.com>
2022-02-23 18:24:22 +0700

    feat(sidebar): add sidebar

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,10 +8,18 @@
       app
     >
       <v-list>
+        <v-list-item class="ma-auto">
+          <v-img max-width="100" :src="require('@/static/logo.svg')"></v-img>
+        </v-list-item>
+      </v-list>
+
+      <v-divider></v-divider>
+      <v-list rounded>
         <v-list-item
           v-for="(item, i) in items"
           :key="i"
           :to="item.to"
+          color="orange darken-2"
           router
           exact
         >
@@ -24,95 +32,100 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
+    <!-- <v-app-bar :clipped-left="clipped" fixed app>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
+      <v-btn icon @click.stop="miniVariant = !miniVariant">
+        <v-icon>mdi-{{ `chevron-${miniVariant ? "right" : "left"}` }}</v-icon>
       </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
+      <v-btn icon @click.stop="clipped = !clipped">
         <v-icon>mdi-application</v-icon>
       </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
+      <v-btn icon @click.stop="fixed = !fixed">
         <v-icon>mdi-minus</v-icon>
       </v-btn>
       <v-toolbar-title v-text="title" />
       <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
+      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
         <v-icon>mdi-menu</v-icon>
       </v-btn>
-    </v-app-bar>
+    </v-app-bar> -->
     <v-main>
       <v-container>
         <Nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
+    <!-- <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
       <v-list>
         <v-list-item @click.native="right = !right">
           <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
+            <v-icon light> mdi-repeat </v-icon>
           </v-list-item-action>
           <v-list-item-title>Switch drawer (click me)</v-list-item-title>
         </v-list-item>
       </v-list>
-    </v-navigation-drawer>
-    <v-footer
-      :absolute="!fixed"
-      app
-    >
+    </v-navigation-drawer> -->
+    <!-- footer -->
+    <!-- <v-footer :absolute="!fixed" app>
       <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
+    </v-footer> -->
   </v-app>
 </template>
 
 <script>
 export default {
-  name: 'DefaultLayout',
-  data () {
+  name: "DefaultLayout",
+  data() {
     return {
       clipped: false,
       drawer: false,
       fixed: false,
       items: [
         {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
+          icon: "mdi-view-dashboard-outline",
+          title: "Overview",
+          to: "/",
         },
         {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
+          icon: "mdi-monitor-screenshot",
+          title: "Screen",
+          to: "/screen",
+        },
+        {
+          icon: "mdi-television-play",
+          title: "Media",
+          to: "/media",
+        },
+        {
+          icon: "mdi-format-list-bulleted",
+          title: "Playlist",
+          to: "/playlist",
+        },
+        {
+          icon: "mdi-calendar-check-outline",
+          title: "Schedule",
+          to: "/schedule",
+        },
+        {
+          icon: "mdi-chart-bar-stacked",
+          title: "Statistics",
+          to: "/statistics",
+        },
+        {
+          icon: "mdi-credit-card-outline",
+          title: "Payouts",
+          to: "/payouts",
+        },
+        {
+          icon: "mdi-cog-outline",
+          title: "Settings",
+          to: "/settings",
+        },
       ],
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js'
-    }
-  }
-}
+      title: "Vuetify.js",
+    };
+  },
+};
 </script>
