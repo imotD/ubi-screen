dde6be9f5f0cd6c3daee664524681fc370ca2f6f
Tommy Alhamra <tommy.alhamra@gmail.com>
2022-02-28 23:51:38 +0700

    test(schedule): try error

--- components/Schedule.vue
+++ components/Schedule.vue
@@ -1,15 +1,42 @@
 <template>
-  <v-row>
+  <v-row class="fill-height">
     <v-col>
-      <v-sheet height="500">
-        <v-calendar ref="calendar" v-model="value" type="week">
-          <!-- <template v-slot:day-body="{ date, week }">
+      <v-sheet height="64">
+        <v-toolbar flat>
+          <v-btn outlined class="mr-4" color="grey darken-2" @click="setToday">
+            Today
+          </v-btn>
+          <v-btn fab text small color="grey darken-2" @click="prev">
+            <v-icon small> mdi-chevron-left </v-icon>
+          </v-btn>
+          <v-btn fab text small color="grey darken-2" @click="next">
+            <v-icon small> mdi-chevron-right </v-icon>
+          </v-btn>
+          <v-toolbar-title v-if="$refs.calendar">
+            {{ $refs.calendar.title }}
+          </v-toolbar-title>
+          <v-spacer></v-spacer>
+        </v-toolbar>
+      </v-sheet>
+      <v-sheet height="600">
+        <v-calendar
+          ref="calendar"
+          v-model="focus"
+          color="primary"
+          type="week"
+          category-show-all
+          :categories="categories"
+          :events="events"
+          :event-color="getEventColor"
+          @change="fetchEvents"
+        >
+          <template v-slot:day-body="{ date, week }">
             <div
               class="v-current-time"
               :class="{ first: date === week[0].date }"
               :style="{ top: nowY }"
             ></div>
-          </template> -->
+          </template>
         </v-calendar>
       </v-sheet>
     </v-col>
@@ -45,10 +72,35 @@ export default {
     ],
     categories: ["John Smith", "Tori Walker"],
   }),
+  computed: {
+    cal() {
+      return this.ready ? this.$refs.calendar : null;
+    },
+    nowY() {
+      return this.cal ? this.cal.timeToY(this.cal.times.now) + "px" : "-10px";
+    },
+  },
   mounted() {
     this.$refs.calendar.checkChange();
+    this.ready = true;
+    this.scrollToTime();
+    this.updateTime();
   },
   methods: {
+    getCurrentTime() {
+      return this.cal
+        ? this.cal.times.now.hour * 60 + this.cal.times.now.minute
+        : 0;
+    },
+    scrollToTime() {
+      const time = this.getCurrentTime();
+      const first = Math.max(0, time - (time % 30) - 30);
+
+      this.cal.scrollToTime(first);
+    },
+    updateTime() {
+      setInterval(() => this.cal.updateTimes(), 60 * 1000);
+    },
     getEventColor(event) {
       return event.color;
     },
@@ -95,5 +147,24 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+.v-current-time {
+  height: 2px;
+  background-color: #ea4335;
+  position: absolute;
+  left: -1px;
+  right: 0;
+  pointer-events: none;
+
+  &.first::before {
+    content: "";
+    position: absolute;
+    background-color: #ea4335;
+    width: 12px;
+    height: 12px;
+    border-radius: 50%;
+    margin-top: -5px;
+    margin-left: -6.5px;
+  }
+}
 </style>
